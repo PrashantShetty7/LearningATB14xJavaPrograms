a93b6212fd1ae89376ce5b71c76cc6ef1394bc34
PrashantShetty7 <shettyprashant850@gmail.com>
2025-11-16 09:46:32 +0530

    16 Novv

---
+++ src/Test/Main.java
@@ -0,0 +1,33 @@
+package Test;
+
+
+/*Create a class "Person" with attributes "name" and "age". Create two instances and print their details.Create a class "Person" with attributes "name" and "age". Create two instances and print their details.
+
+Create Person objects with names "John" (25) and "Alice" (30)
+*/
+
+class Person
+{
+    String name;
+    int age;
+
+
+    Person(String name,int age){
+        this.name = name;
+        this.age = age;
+    }
+    void display(){
+        System.out.println("Name: " + name + ",Age: " +age);
+    }
+}
+public class Main{
+    public static void main(String[] args){
+        Person p1= new Person("John",25);
+        Person p2= new Person("Alice",38);
+
+        p1.display();
+        p2.display();
+
+
+    }
+}
